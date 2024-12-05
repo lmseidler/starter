8ee653dda4dd292bec6dfb316013f1cff2d1bebb
lmseidler <seidler118@gmail.com>
2024-12-05 12:18:40 +0100

    fortran autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fortran",
+  callback = function()
+    vim.bo.shiftwidth = 3 -- change to your preferred number of spaces
+    vim.bo.tabstop = 3 -- change to your preferred number of spaces
+  end,
+})
