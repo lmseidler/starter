4d5e8dcef09906d4392dcd80ccde8cd163bf336f
lmseidler <seidler118@gmail.com>
2025-07-07 22:09:55 +0200

    made codecompanion with vectorcode work (config: install copilot and vectorcode cli)

--- lua/plugins/companion.lua
+++ lua/plugins/companion.lua
@@ -41,13 +41,9 @@ return {
             collapse = false, -- whether the individual tools should be shown in the chat
           },
           -- tool_opts = {
-          --   ---@type VectorCode.CodeCompanion.ToolOpts
           --   ["*"] = {},
-          --   ---@type VectorCode.CodeCompanion.LsToolOpts
           --   ls = {},
-          --   ---@type VectorCode.CodeCompanion.VectoriseToolOpts
           --   vectorise = {},
-          --   ---@type VectorCode.CodeCompanion.QueryToolOpts
           --   query = {
           --     max_num = { chunk = -1, document = -1 },
           --     default_num = { chunk = 50, document = 10 },
@@ -55,9 +51,7 @@ return {
           --     use_lsp = false,
           --     no_duplicate = true,
           --     chunk_mode = false,
-          --     ---@type VectorCode.CodeCompanion.SummariseOpts
           --     summarise = {
-          --       ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
           --       enabled = false,
           --       adapter = nil,
           --       query_augmented = true,
