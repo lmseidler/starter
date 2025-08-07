c52993052ef0825e33cf058520266752a4b787c2
lmseidler <seidler118@gmail.com>
2025-08-07 15:11:43 +0200

    disabled avante for now

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,94 +1,94 @@
-return {
-  "yetone/avante.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "stevearc/dressing.nvim", -- for input provider dressing
-    "folke/snacks.nvim", -- for input provider snacks
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  -- ⚠️ must add this setting! ! !
-  build = function()
-    -- conditionally use the correct build system for the current OS
-    if vim.fn.has("win32") == 1 then
-      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-    else
-      return "make"
-    end
-  end,
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  ---@module 'avante'
-  ---@type avante.Config
-  opts = {
-    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-    ---@type Provider
-    provider = "copilot", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
-    ---@alias Mode "agentic" | "legacy"
-    ---@type Mode
-    mode = "agentic", -- The default mode for interaction. "agentic" uses tools to automatically generate code, "legacy" uses the old planning method to generate code.
-    -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
-    -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
-    -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
-    auto_suggestions_provider = "claude",
-    providers = {
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-5-sonnet-20241022",
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 4096,
-        },
-      },
-      copilot = {
-        -- model = "claude-sonnet-4",
-        model = "gpt-4.1",
-        timeout = 30000,
-      },
-      openrouter = {
-        __inherited_from = "openai", -- 🔑 This is crucial!
-        api_key_name = "OPENROUTER_API_KEY", -- Environment variable name
-        endpoint = "https://openrouter.ai/api/v1",
-        -- model = "moonshotai/kimi-k2",
-        model = "qwen/qwen3-coder",
-        timeout = 30000,
-        extra_request_body = {
-          temperature = 0.7,
-          max_tokens = 4096,
-        },
-      },
-    },
-  },
-}
+-- return {
+--   "yetone/avante.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+--     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--     "stevearc/dressing.nvim", -- for input provider dressing
+--     "folke/snacks.nvim", -- for input provider snacks
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = true,
+--         },
+--       },
+--     },
+--     {
+--       -- Make sure to set this up properly if you have lazy=true
+--       "MeanderingProgrammer/render-markdown.nvim",
+--       opts = {
+--         file_types = { "markdown", "Avante" },
+--       },
+--       ft = { "markdown", "Avante" },
+--     },
+--   },
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   -- ⚠️ must add this setting! ! !
+--   build = function()
+--     -- conditionally use the correct build system for the current OS
+--     if vim.fn.has("win32") == 1 then
+--       return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+--     else
+--       return "make"
+--     end
+--   end,
+--   event = "VeryLazy",
+--   version = false, -- Never set this value to "*"! Never!
+--   ---@module 'avante'
+--   ---@type avante.Config
+--   opts = {
+--     ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+--     ---@type Provider
+--     provider = "copilot", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+--     ---@alias Mode "agentic" | "legacy"
+--     ---@type Mode
+--     mode = "agentic", -- The default mode for interaction. "agentic" uses tools to automatically generate code, "legacy" uses the old planning method to generate code.
+--     -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+--     -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+--     -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+--     auto_suggestions_provider = "claude",
+--     providers = {
+--       claude = {
+--         endpoint = "https://api.anthropic.com",
+--         model = "claude-3-5-sonnet-20241022",
+--         extra_request_body = {
+--           temperature = 0.75,
+--           max_tokens = 4096,
+--         },
+--       },
+--       copilot = {
+--         -- model = "claude-sonnet-4",
+--         model = "gpt-4.1",
+--         timeout = 30000,
+--       },
+--       openrouter = {
+--         __inherited_from = "openai", -- 🔑 This is crucial!
+--         api_key_name = "OPENROUTER_API_KEY", -- Environment variable name
+--         endpoint = "https://openrouter.ai/api/v1",
+--         -- model = "moonshotai/kimi-k2",
+--         model = "qwen/qwen3-coder",
+--         timeout = 30000,
+--         extra_request_body = {
+--           temperature = 0.7,
+--           max_tokens = 4096,
+--         },
+--       },
+--     },
+--   },
+-- }
