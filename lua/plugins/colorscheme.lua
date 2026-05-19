a2862cdcc0aa8880f0d3b90fd8d9ac94e357b3d2
lmseidler <seidler118@gmail.com>
2026-05-19 15:50:52 +0200

    play with colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,24 @@
 return {
-  { "loctvl842/monokai-pro.nvim", opts = { filter = "octagon" } },
-  { "navarasu/onedark.nvim" },
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      local mp = require("monokai-pro")
+
+      mp.setup({
+        override_palette = function(filter)
+          local palette = mp.get_palette("octagon")
+
+          palette.background = "#101014"
+
+          return palette
+        end,
+      })
+    end,
+    config = function()
+      vim.cmd.colorscheme("monokai-pro")
+    end,
+  },
   { "LazyVim/LazyVim", opts = { colorscheme = "monokai-pro" } },
 }
